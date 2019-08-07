5ea30c0f62eba1aa6a72bc1ad0073e7a973aa74f
Carl Levine Jr <clevine313@gmail.com>
2019-08-07 15:16:25 -0400

    pretty good

---
+++ Cardless.ATM/src/main/java/MoneyGrabbers/Cardless/ATM/Application.java
@@ -0,0 +1,13 @@
+package MoneyGrabbers.Cardless.ATM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
